bdd01cd69c6086ae58a36b9518474e9958eac63c
Hitesh Garg <garg95hitesh@gmail.com>
2017-06-02 00:27:08 +0530

    admintools app created

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="admintools",
+      version='0.1',
+      license="MIT",
+      description="opencivicdata admin tools",
+      long_description="",
+      url="",
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+      	  'Django==1.11',	
+      ],
+      classifiers=["Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6",
+                   ],
+      )
