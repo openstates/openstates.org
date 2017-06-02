c9fed4bdda740ed13328c87835c18eda3d68ab47
Hitesh Garg <garg95hitesh@gmail.com>
2017-06-02 17:37:45 +0530

    model class for data quality issues

---
+++ admintools/common.py
@@ -0,0 +1,15 @@
+
+ALERT_CHOICES = (
+    ('error', 'Error'),
+    ('warning', 'Warning'),
+)
+
+ISSUE_CHOICES = (
+    # Person
+    ('address', 'Missing Postal Address'),
+    ('email', 'Missing Email'),
+    ('voice', 'Missing Voice Phone'),
+    ('image', 'Missing Image URL'),
+
+    # Votes
+)
--- admintools/models.py
+++ admintools/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .common import ALERT_CHOICES, ISSUE_CHOICES
 
-# Create your models here.
+
+class DataQualityIssues(models.Model):
+    content_type = models.ForeignKey(ContentType,
+                                     related_name="dataquality_issues")
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    alert = models.CharField(max_length=50, choices=ALERT_CHOICES)
+    issue = models.CharField(max_length=50, choices=ISSUE_CHOICES)
+
+    class Meta:
+        db_table = 'opencivicdata_dataqualityissues'
+        index_together = [
+            ['alert', 'issue']
+        ]
+
+    def __str__(self):
+        return '{} to {} - {}'.format(self.get_name_display(),
+                                      type(self.content_object).__name__,
+                                      self.get_alert_display())
