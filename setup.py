0d253466742823537c436ff48e07384e2c7a5420
Hitesh Garg <garg95hitesh@gmail.com>
2017-07-14 22:21:51 +0530

    dropped dependency_links

--- setup.py
+++ setup.py
@@ -12,11 +12,8 @@ setup(name="admintools",
       install_requires=[
             'Django>=1.11',
             'pupa>=0.6',
-            'opencivicdata',
+            'opencivicdata>=1.0',
             'flake8',
-      ],
-      dependency_links=[
-            'git+https://github.com/opencivicdata/python-opencivicdata.git@879d73b94557c63332f0c7ca2d70984810c64734#egg=opencivicdata-dev'
       ],
       classifiers=["Intended Audience :: Developers",
                    "License :: OSI Approved :: MIT License",
