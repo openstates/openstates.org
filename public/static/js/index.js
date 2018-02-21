8ad355f5cc9e4f2675c97e30a49fb601cd1e601d
Olivia Cheng <olivia@heyitsolivia.com>
2018-02-20 21:53:20 -0500

    Set up React and Babel to compile it

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,3 +1,12 @@
 import '../styles/app.scss';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <p>Open States + React</p>,
+  document.getElementById('root')
+);
+
+
 console.log('Hello, Open States!');
