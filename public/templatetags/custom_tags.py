46b236d512f59d4c85bbb8cd2243c62abc4c52d6
Miles Watkins <miles.w.watkins@gmail.com>
2018-03-01 16:22:30 -0500

    Create vote card template, and add to legislator view

--- public/templatetags/custom_tags.py
+++ public/templatetags/custom_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.inclusion_tag('public/components/sources.html')
 def sources(sources_object):
     return sources_object
+
+
+@register.inclusion_tag('public/components/vote-card.html')
+def vote_card(vote_card_object):
+    return vote_card_object
--- public/views.py
+++ public/views.py
@@ -43,7 +43,7 @@ def legislator(request, state, legislator_id):
     ]
 
     # TO DO
-    votes = []
+    votes = MOCK_VOTES
 
     legislature = legislative_membership.organization.parent or legislative_membership.organization
     jurisdiction = legislature.jurisdiction
@@ -79,3 +79,47 @@ def legislator(request, state, legislator_id):
             'sources': sources
         }
     )
+
+
+MOCK_VOTES = {
+    '2018-02-15': [
+        {
+            'bill_identifier': 'HB 1012',
+            'motion_text': 'Read the third time and passed and ordered transmitted to the Senate.',
+            'voter_name': 'Olszewski',
+            'legislator_vote': 'yes',
+            'counts': {
+                'yes': 86,
+                'no': 1,
+                'other': 0,
+                'total': 87
+            }
+        }
+    ],
+    '2017-04-11': [
+        {
+            'bill_identifier': 'HB 6',
+            'motion_text': 'Do Concur',
+            'voter_name': 'Olszewski',
+            'legislator_vote': 'yes',
+            'counts': {
+                'yes': 50,
+                'no': 0,
+                'other': 0,
+                'total': 50
+            }
+        },
+        {
+            'bill_identifier': 'HB 83',
+            'motion_text': 'Passage, Third Reading',
+            'voter_name': 'Olszewski',
+            'legislator_vote': 'no',
+            'counts': {
+                'yes': 110,
+                'no': 4,
+                'other': 6,
+                'total': 120
+            }
+        }
+    ]
+}
