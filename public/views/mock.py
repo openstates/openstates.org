c58074063e047c8cacf8a8c9944825574abc3ed7
Miles Watkins <miles.w.watkins@gmail.com>
2018-03-02 14:59:18 -0500

    Simplify vote card, including passage outcome

--- public/views/mock.py
+++ public/views/mock.py
@@ -8,12 +8,7 @@ LEGISLATOR_VOTES = {
             'motion_text': 'Read the third time and passed and ordered transmitted to the Senate.',
             'voter_name': 'Olszewski',
             'legislator_vote': 'yes',
-            'counts': {
-                'yes': 86,
-                'no': 1,
-                'other': 0,
-                'total': 87
-            }
+            'result': 'pass'
         }
     ],
     '2017-04-11': [
@@ -22,24 +17,14 @@ LEGISLATOR_VOTES = {
             'motion_text': 'Do Concur',
             'voter_name': 'Olszewski',
             'legislator_vote': 'yes',
-            'counts': {
-                'yes': 50,
-                'no': 0,
-                'other': 0,
-                'total': 50
-            }
+            'result': 'fail'
         },
         {
             'bill_identifier': 'HB 83',
             'motion_text': 'Passage, Third Reading',
             'voter_name': 'Olszewski',
             'legislator_vote': 'no',
-            'counts': {
-                'yes': 110,
-                'no': 4,
-                'other': 6,
-                'total': 120
-            }
+            'result': 'pass'
         }
     ]
 }
