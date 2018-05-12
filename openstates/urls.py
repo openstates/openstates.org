5e283e04ea3210a4d12608deac8c4316969db93c
James Turk <james.p.turk@gmail.com>
2018-05-12 01:03:25 -0400

    not needed in deployed version

--- openstates/urls.py
+++ openstates/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     url(r'^djadmin/', admin.site.urls),
     url(r'^djadmin/', include('opencivicdata.core.admin.urls')),
     url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    url(r'^public/', include('public.urls')),
-    url(r'^reports/', include('dataquality.urls'))
+    # url(r'^public/', include('public.urls')),
+    # url(r'^reports/', include('dataquality.urls'))
 ]
 
 
