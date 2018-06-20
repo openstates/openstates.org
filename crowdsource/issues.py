60afa0603ae9a425fed8420086b4bef50ba43ae2
Bikram Bharti <bikram.bharti99@gmail.com>
2018-06-20 09:36:57 +0530

    new issues added for crowdsource

---
+++ crowdsource/issues.py
@@ -0,0 +1,7 @@
+from dataquality.issues import IssueType
+
+# More Person issues
+IssueType('wrong-phone', 'Wrong Phone Number', 'person', 'error')
+IssueType('wrong-email', 'Wrong Email', 'person', 'error')
+IssueType('wrong-address', 'Wrong Postal Address', 'person', 'error')
+IssueType('wrong-photo', 'Wrong Photo', 'person', 'error')
