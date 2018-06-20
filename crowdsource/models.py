7e45b2ec87d0eba14392906f43e40c173f37cee6
Bikram Bharti <bikram.bharti99@gmail.com>
2018-06-20 09:38:49 +0530

    new model for crowdsource issue

--- crowdsource/models.py
+++ crowdsource/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from opencivicdata.core.models import Jurisdiction
 
-# Create your models here.
+from . import issues
+
+
+class CrowdSourceIssue(models.Model):
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('ignored', 'Ignored')
+    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.CharField(max_length=300)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    jurisdiction = models.ForeignKey(Jurisdiction, related_name="crowdsource_issues",
+                                     on_delete=models.CASCADE)
+    issue = models.CharField(max_length=150, choices=issues.IssueType.choices())
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+    message = models.TextField(blank=True)
+
+    reporter_email = models.EmailField(blank=True)
+    reporter_name = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return '{} issue type'.format(self.issue)
