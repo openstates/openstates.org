3934fce01543f02a08279cdb821e7ae96368f512
Bikram Bharti <bikram.bharti99@gmail.com>
2018-06-26 02:37:50 +0530

    views for resolver added

--- crowdsource/views.py
+++ crowdsource/views.py
@@ -4,8 +4,8 @@ from django.db.models import Count
 from opencivicdata.legislative.models import Bill, VoteEvent
 
 from .issues import IssueType
-from .models import CrowdSourceIssue
-from .forms import IssueForm
+from .models import CrowdSourceIssue, CrowdSourceIssueResolver
+from .forms import IssueForm, ResolverForm
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 
@@ -40,16 +40,40 @@ def report_issue(request):
             issue.content_object = reporter_utility(issue_type, issue.object_id)
             issue.save()
             messages.success(request, "Issue Successfully Created")
-            return render(request, 'report.html', {'form': form})
+            return render(request, 'report.html', {'form': form, 'headline': "New Issue"})
         else:
             errors = post_form.errors.as_data()
             for error in errors:
                 messages.error(request, error+": "+errors[error])
             if active_issue_exists:
                 messages.error(request, "Issue for the object already exists.")
-            return render(request, 'report.html', {'form': post_form})
+            return render(request, 'report.html', {'form': post_form, 'headline': "New Issue"})
         
     else:
-        return render(request, 'report.html', {'form': form})
+        return render(request, 'report.html', {'form': form, 'headline': "New Issue"})
         
         
+def submit_resolve(request):
+
+    form = ResolverForm()
+    if request.method == 'POST':
+        post = request.POST
+        post_form = ResolverForm(post)
+        if post_form.is_valid():
+            resolver = post_form.save(commit=False)
+            exist_resolver = CrowdSourceIssueResolver.objects.filter(issue = resolver.issue,
+                                                new_value=resolver.new_value)
+            if exist_resolver.count() > 0:
+                messages.error(request, "Resolver with the updates already exists.")
+                return render(request, 'report.html', {'form': post_form, 'headline': "New Resolver"})
+            resolver.save()
+            messages.success(request, "Resolve Submitted Successfully !")
+            return render(request, 'report.html', {'form': form, 'headline': "New Resolver"})
+        else:
+            errors = post_form.errors.as_data()
+            for error in errors:
+                messages.error(request, error+": "+errors[error])
+            return render(request, 'report.html', {'form': post_form, 'headline': "New Resolver"})
+        
+    else:
+        return render(request, 'report.html', {'form': form, 'headline': "New Resolver"})
