401ce88af3759f8a67e0681680902ef2b84ca75e
Bikram Bharti <bikram.bharti99@gmail.com>
2018-06-28 04:11:27 +0530

    views for reporting changed for dataqualityIssue model

--- crowdsource/views.py
+++ crowdsource/views.py
@@ -2,25 +2,28 @@ from django.shortcuts import render
 
 from django.db.models import Q
 from opencivicdata.legislative.models import Bill, VoteEvent
+from opencivicdata.core.models import Jurisdiction, Person
 
 from .issues import IssueType
-from .models import CrowdSourceIssue, CrowdSourceIssueResolver
+from dataquality.models import DataQualityIssue, IssueResolverPatch
 from .forms import IssueForm, ResolverForm
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 
 
-def reporter_utility(issue_type, identifier):
+def get_object_by_identifier(issue_type, identifier):
     content_object = None
     if issue_type == 'bill':
-        content_object = Bill.objects.get(id=identifier)
+        content_object = Bill.objects.filter(id=identifier)
     elif issue_type == 'voteevent':
-        content_object = VoteEvent.objects.get(id=identifier)
+        content_object = VoteEvent.objects.filter(id=identifier)
+    elif issue_type == 'person':
+        content_object = Person.objects.filter(id=identifier)
     return content_object
 
 def already_exists(object_id,issue):
         try:
-            CrowdSourceIssue.objects.get(object_id=object_id, issue=issue,
+            DataQualityIssue.objects.get(object_id=object_id, issue=issue,
                                          status='active')
             return True
         except:
@@ -37,7 +40,12 @@ def report_issue(request):
         if post_form.is_valid() and not active_issue_exists:
             issue = post_form.save(commit=False)
             issue_type = IssueType.class_for(issue.issue)
-            issue.content_object = reporter_utility(issue_type, issue.object_id)
+            content_object = get_object_by_identifier(issue_type, issue.object_id)
+            if content_object:
+                issue.content_object = content_object[0]
+            else:
+                messages.error(request, "Object with '%s' object id not found."%issue.object_id) 
+                return render(request, 'report.html', {'form': post_form, 'headline': "NewIssue"})
             issue.save()
             messages.success(request, "Issue Successfully Created")
             return render(request, 'report.html', {'form': form, 'headline': "New Issue"})
@@ -61,6 +69,7 @@ def submit_resolve(request):
         post_form = ResolverForm(post)
         if post_form.is_valid():
             resolver = post_form.save(commit=False)
+            # user with one email can only create one resolver for a particular issue, otherwise he can edit it
             exist_resolver = CrowdSourceIssueResolver.objects.filter(Q(issue = resolver.issue,
                                                 new_value=resolver.new_value) | Q(issue = resolver.issue,
                                                 reporter_email=resolver.reporter_email))
@@ -78,4 +87,5 @@ def submit_resolve(request):
         
     else:
         return render(request, 'report.html', {'form': form, 'headline': "New Resolver"})
+
 
