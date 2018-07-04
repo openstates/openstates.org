915f7fb9317cebfee51541c1b8bc61b99185cf64
Bikram Bharti <bikram.bharti99@gmail.com>
2018-07-04 05:50:18 +0530

    urls for listing issues added

--- crowdsource/urls.py
+++ crowdsource/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 
     url(r'^report/$', views.report_issue, name='report'),
     url(r'^resolve/$', views.submit_resolve, name='resolve'),
+    url(r'^issues/$', views.list_issues, name='list'),
 ]
