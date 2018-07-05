83b65a11826f6ec634942285cf0e444becc612ed
Bikram Bharti <bikram.bharti99@gmail.com>
2018-07-05 05:57:43 +0530

    tests for issue reporting added

--- crowdsource/tests/test_views.py
+++ crowdsource/tests/test_views.py
@@ -19,35 +19,129 @@ class BaseViewTestCase(TestCase):
         )
         self.person = Person.objects.create(name='Bikram Bharti')
 
+
 class ReportTests(BaseViewTestCase):
     
     def test_view_response(self):
         response = self.client.get(reverse('report'))
         self.assertEqual(response.status_code, 200)
 
+    # valid Issue should be added
+    def test_view_valid_issue(self):
+        pre_issue_count = DataQualityIssue.objects.all().count()
+        data = {
+            'jurisdiction': self.jur.id,
+            'object_id': self.person.id,
+            'issue': 'wrong-email'
+        }
+        response = self.client.post(reverse('report'), data)
+
+        post_issue_count = DataQualityIssue.objects.all().count()
+        issue = DataQualityIssue.objects.get(jurisdiction = self.jur.id, 
+                                             object_id=self.person.id,
+                                             issue='wrong-email')
+        self.assertEqual(pre_issue_count+1, post_issue_count)
+
+    # issue should not be added, with invalid object_id
+    def test_view_invalid_object_id(self):
+        
+        pre_issue_count = DataQualityIssue.objects.all().count()
+        data = {
+            'jurisdiction': self.jur.id,
+            'object_id': "invalid_object",
+            'issue': 'wrong-email'
+        }
+        response = self.client.post(reverse('report'), data)
+
+        post_issue_count = DataQualityIssue.objects.all().count()
+        issue = DataQualityIssue.objects.filter(object_id = "invalid_object")
+
+        self.assertEqual(pre_issue_count, post_issue_count)
+        self.assertEqual(issue.count(), 0)
+
+    # issue should not be added, with invalid jurisdiction
+    def test_view_invalid_object_id(self):
+        
+        pre_issue_count = DataQualityIssue.objects.all().count()
+        data = {
+            'jurisdiction': "invalid-jurisdiction",
+            'object_id': self.person.id,
+            'issue': 'wrong-email'
+        }
+        response = self.client.post(reverse('report'), data)
+
+        post_issue_count = DataQualityIssue.objects.all().count()
+        issue = DataQualityIssue.objects.filter(jurisdiction__id = "invalid-jurisdiction")
+
+        self.assertEqual(pre_issue_count, post_issue_count)
+        self.assertEqual(issue.count(), 0)
+
+    # issue should not be added, with invalid issue
+    def test_view_invalid_issue(self):
+
+        pre_issue_count = DataQualityIssue.objects.all().count()
+        data = {
+            'jurisdiction': self.jur.id,
+            'object_id': self.person.id,
+            'issue': 'invalid-issue'
+        }
+        response = self.client.post(reverse('report'), data)
+
+        post_issue_count = DataQualityIssue.objects.all().count()
+        issue = DataQualityIssue.objects.filter(issue = "invalid-issue")
+
+        self.assertEqual(pre_issue_count, post_issue_count)
+        self.assertEqual(issue.count(), 0)
+
+    def test_view_issue_already_exist(self):
+        
+        pre_issue_count = DataQualityIssue.objects.all().count()
+        data = {
+            'jurisdiction': self.jur.id,
+            'object_id': self.person.id,
+            'issue': 'wrong-phone'
+        }
+        response = self.client.post(reverse('report'), data)
+
+        post_issue_count = DataQualityIssue.objects.all().count()
+        issue = DataQualityIssue.objects.get(issue = "wrong-phone",
+                                             object_id = self.person.id)
+        
+        self.assertEqual(pre_issue_count+1, post_issue_count)
+        pre_issue_count = post_issue_count
+
+        issue = DataQualityIssue.objects.get(issue = "wrong-phone",
+                                             object_id = self.person.id)
+
+        post_issue_count = DataQualityIssue.objects.all().count()
+        self.assertEqual(pre_issue_count, post_issue_count)
+
+
 class ResolveTests(BaseViewTestCase):
     
     def test_view_response(self):
-        response = self.client(reverse('resolve'))
+        response = self.client.get(reverse('resolve'))
         self.assertEqual(response.status_code, 200)
 
 class ListTests(BaseViewTestCase):
 
     def test_view_response(self):
-        response = self.client(reverse('list'))
+        response = self.client.get(reverse('list'))
         self.assertEqual(response.status_code, 200)
     
     def test_issue_count(self):
-        response = self.client(reverse('list'))
-        assertEqual(len(response.context['issues']), 0)
+        response = self.client.get(reverse('list'))
+        self.assertEqual(len(response.context['issues']), 0)
         issue = DataQualityIssue.objects.create(content_object=self.person,
-                                                Jurisdiction=self.jur,
+                                                jurisdiction=self.jur,
                                                 issue='wrong-email')
-        assertEqual(len(response.context['issues']), 1)
+        response = self.client.get(reverse('list'))
+        self.assertEqual(len(response.context['issues']), 1)
         issue = DataQualityIssue.objects.create(content_object=self.person,
-                                                Jurisdiction=self.jur,
+                                                jurisdiction=self.jur,
                                                 issue='wrong-phone')
-        assertEqual(len(response.context['issues']), 2)
+        response = self.client.get(reverse('list'))
+        self.assertEqual(len(response.context['issues']), 2)
 
 
     
