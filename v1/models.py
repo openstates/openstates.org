0d9ccd4b88e90064692f36dedf64aa1b67888d83
James Turk <james.p.turk@gmail.com>
2018-10-28 20:39:08 -0400

    add legacy bill ids for v1

---
+++ v1/models.py
@@ -0,0 +1,7 @@
+from django.contrib.gis.db import models
+from opencivicdata.legislative.models import Bill
+
+
+class LegacyBillMapping(models.Model):
+    legacy_id = models.CharField(max_length=20, primary_key=True)
+    bill = models.ForeignKey(Bill, related_name='legacy_mapping', on_delete=models.CASCADE)
