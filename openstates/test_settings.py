a882795b0b69f527896763c9acf54c86e21494a8
James Turk <dev@jamesturk.net>
2018-12-19 16:39:17 -0500

    fixes for issues related to testing/whitenoise

--- openstates/test_settings.py
+++ openstates/test_settings.py
@@ -1,3 +1,8 @@
 from .settings import *  # noqa
 
 SIMPLEKEYS_ZONE_PATHS = []
+
+MIDDLEWARE_REMOVE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+MIDDLEWARE = [m for m in MIDDLEWARE if m not in MIDDLEWARE_REMOVE]      # noqa
