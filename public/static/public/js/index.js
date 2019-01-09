31d1407ff2d87b01d1aa27511b83b6cc07c7721a
James Turk <dev@jamesturk.net>
2019-01-09 12:54:50 -0500

    swap out svg logo for png on IE

--- public/static/public/js/index.js
+++ public/static/public/js/index.js
@@ -25,6 +25,7 @@ function addBanner(content) {
 function checkIE() {
   var ua = window.navigator.userAgent;
   if (ua.indexOf('MSIE') > 0 || ua.indexOf("Trident/") > 0) {
+    document.querySelector(".header__logo").src = "/static/public/images/openstates_logo.png";
     addBanner("You are viewing this site in an outdated browser, some features may not work.");
   }
 }
