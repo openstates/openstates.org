f9425df10538f11f673e0f69c958628f38a0c759
James Turk <dev@jamesturk.net>
2020-01-02 13:53:20 -0500

    add note that accounts are in beta

--- public/views/account.py
+++ public/views/account.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 @login_required
 def profile(request):
     primary = request.user.emailaddress_set.get(primary=True)
+    messages.info(
+        request,
+        "Accounts are currently a beta feature, more functionality is coming soon!",
+    )
     return render(request, "account/profile.html", {"primary_email": primary})
