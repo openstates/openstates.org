53ae776b75ff4dfce497df1fbcdcc15c18cfab9f
James Turk <dev@jamesturk.net>
2020-01-12 12:58:21 -0500

    export_csv: creating directories and renaming fields

--- public/management/commands/export_csv.py
+++ public/management/commands/export_csv.py
@@ -1,5 +1,7 @@
+import os
 import csv
 from django.core.management.base import BaseCommand
+from django.db.models import F
 from opencivicdata.legislative.models import (
     LegislativeSession,
     Bill,
@@ -17,6 +19,7 @@ from opencivicdata.legislative.models import (
     VoteEvent,
     PersonVote,
     VoteCount,
+    VoteSource,
 )
 from utils.common import abbr_to_jid
 
@@ -37,15 +40,16 @@ def export_session(state, session):
     )
     bills = Bill.objects.filter(legislative_session=sobj).values(
         "id",
-        "legislative_session__identifier",
-        "legislative_session__jurisdiction__name",
         "identifier",
         "title",
-        "from_organization__classification",
         "classification",
         "subject",
+        session_identifier=F("legislative_session__identifier"),
+        jurisdiction=F("legislative_session__jurisdiction__name"),
+        organization_classification=F("from_organization__classification"),
     )
-    export_csv(f"{state}_{session}_bills.csv", bills)
+    os.makedirs(f"{state}/{session}")
+    export_csv(f"{state}/{session}/{state}_{session}_bills.csv", bills)
 
     for Model, fname in (
         (BillAbstract, "bill_abstracts"),
@@ -59,16 +63,16 @@ def export_session(state, session):
         (BillVersion, "bill_versions"),
     ):
         subobjs = Model.objects.filter(bill__legislative_session=sobj).values()
-        export_csv(f"{state}_{session}_{fname}.csv", subobjs)
+        export_csv(f"{state}/{session}/{state}_{session}_{fname}.csv", subobjs)
 
     subobjs = BillDocumentLink.objects.filter(
         document__bill__legislative_session=sobj
     ).values()
-    export_csv(f"{state}_{session}_bill_document_links.csv", subobjs)
+    export_csv(f"{state}/{session}/{state}_{session}_bill_document_links.csv", subobjs)
     subobjs = BillVersionLink.objects.filter(
         version__bill__legislative_session=sobj
     ).values()
-    export_csv(f"{state}_{session}_bill_version_links.csv", subobjs)
+    export_csv(f"{state}/{session}/{state}_{session}_bill_version_links.csv", subobjs)
 
     # TODO: BillActionRelatedEntity
 
@@ -81,22 +85,44 @@ def export_session(state, session):
         "start_date",
         "result",
         "organization_id",
-        "legislative_session__jurisdiction__name",
-        "legislative_session__identifier",
         "bill_id",
         "bill_action_id",
+        jurisdiction=F("legislative_session__jurisdiction__name"),
+        session_identifier=F("legislative_session__identifier"),
     )
-    export_csv(f"{state}_{session}_votes.csv", votes)
+    export_csv(f"{state}/{session}/{state}_{session}_votes.csv", votes)
     for Model, fname in (
         (PersonVote, "vote_people"),
         (VoteCount, "vote_counts"),
+        (VoteSource, "vote_sources"),
     ):
         subobjs = Model.objects.filter(vote_event__legislative_session=sobj).values()
-        export_csv(f"{state}_{session}_{fname}.csv", subobjs)
+        export_csv(f"{state}/{session}/{state}_{session}_{fname}.csv", subobjs)
 
 
 class Command(BaseCommand):
     help = "export data as CSV"
 
+    def add_arguments(self, parser):
+        parser.add_argument("state")
+        parser.add_argument("sessions", nargs="*")
+        parser.add_argument("--all", action="store_true")
+
     def handle(self, *args, **options):
-        export_session("nc", "2019")
+        state = options["state"]
+        sessions = [
+            s.identifier
+            for s in LegislativeSession.objects.filter(
+                jurisdiction_id=abbr_to_jid(state)
+            )
+        ]
+        if options["all"]:
+            options["sessions"] = sessions
+        if not options["sessions"]:
+            print("available sessions:")
+            for session in sessions:
+                print("    ", session)
+        else:
+            for session in options["sessions"]:
+                if session in sessions:
+                    export_session(state, session)
