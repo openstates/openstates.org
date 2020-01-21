60ada65e5026aca9dd8d227c66af9bf1d01e2138
James Turk <dev@jamesturk.net>
2020-01-21 14:59:46 -0500

    add Subscription model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from profiles.models import Profile
+from profiles.models import Profile, Subscription
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("user",)
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "user", "subscription_type")
+    autocomplete_fields = ("sponsor", "bill")
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+from opencivicdata.core.models import Person
+from opencivicdata.legislative.models import Bill
 
 
 class Profile(models.Model):
@@ -13,3 +16,51 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile for {self.user}"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="subscriptions"
+    )
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    query = models.CharField(max_length=300, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    chamber = models.CharField(max_length=15, blank=True)
+    session = models.CharField(max_length=100, blank=True)
+    classification = models.CharField(max_length=50, blank=True)
+    subjects = ArrayField(models.CharField(max_length=100), blank=True)
+    status = ArrayField(models.CharField(max_length=50), blank=True)
+    sponsor = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        null=True,
+        blank=True,
+    )
+    bill = models.ForeignKey(
+        Bill,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        null=True,
+        blank=True,
+    )
+
+    @property
+    def subscription_type(self):
+        if self.bill:
+            return "bill"
+        elif self.query:
+            return "query"
+        elif self.sponsor:
+            return "sponsor"
+        raise ValueError("invalid subscription")
+
+    def __str__(self):
+        if self.subscription_type == "bill":
+            return f"{self.user} subscription to {self.bill}"
+        elif self.subscription_type == "query":
+            return f"{self.user} subscription to '{self.query}'"
+        elif self.subscription_type == "sponsor":
+            return f"{self.user} subscription to '{self.sponsor}'"
