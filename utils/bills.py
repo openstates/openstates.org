1fa8b104d0e0dd46ccac9de815728e4a7252a070
James Turk <dev@jamesturk.net>
2020-02-13 11:27:18 -0500

    remove dead get_bills_with_action_annotation code

--- utils/bills.py
+++ utils/bills.py
@@ -1,6 +1,5 @@
 import re
-from django.db.models import Min, Max, OuterRef, Subquery
-from opencivicdata.legislative.models import Bill, BillAction
+from opencivicdata.legislative.models import Bill
 from .websearchquery import WebSearchQuery as SearchQuery
 from .common import abbr_to_jid
 
@@ -14,30 +13,6 @@ def fix_bill_id(bill_id):
     if _mi_bill_id_re.match(bill_id):
         return _mi_bill_id_re.sub(r"\1 \2", bill_id, 1).strip()
     return _bill_id_re.sub(r"\1 \2", bill_id, 1).strip()
-
-
-def get_bills_with_action_annotation():
-    """
-    return Bill queryset that is already annotated with:
-        first_action_date
-        latest_action_date
-        latest_action_description
-
-    and legislative_session & jurisdiction in the select_related already
-    """
-    latest_actions = (
-        BillAction.objects.filter(bill=OuterRef("pk"))
-        .order_by("date")
-        .values("description")[:1]
-    )
-    return (
-        Bill.objects.all()
-        .annotate(first_action_date=Min("actions__date"))
-        .annotate(latest_action_date=Max("actions__date"))
-        .annotate(latest_action_description=Subquery(latest_actions))
-        .select_related("legislative_session", "legislative_session__jurisdiction")
-        .prefetch_related("actions")
-    )
 
 
 def search_bills(
