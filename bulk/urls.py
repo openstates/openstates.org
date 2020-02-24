3c35f1fa0360979f8360b65889892e24bb980d54
James Turk <dev@jamesturk.net>
2020-02-24 17:22:55 -0500

    require login to get download links

--- bulk/urls.py
+++ bulk/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.overview),
-    path("session-csv/", views.bulk_csv_list),
-    path("session-json/", views.bulk_json_list),
+    path("session-csv/", views.bulk_session_list, {"data_type": "csv"}),
+    path("session-json/", views.bulk_session_list, {"data_type": "json"}),
 ]
--- bulk/views.py
+++ bulk/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import DataExport
 
 
@@ -6,19 +7,16 @@ def overview(request):
     return render(request, "bulk/overview.html", {})
 
 
-def bulk_csv_list(request):
+def bulk_session_list(request, data_type):
+    if request.user.is_anonymous:
+        messages.warning(request, "Please log in to access download links.")
     exports = (
-        DataExport.objects.filter(data_type="csv")
+        DataExport.objects.filter(data_type=data_type)
         .select_related("session", "session__jurisdiction")
         .order_by("session__jurisdiction__name", "session__name")
     )
-    return render(request, "bulk/bulk_csv_list.html", {"exports": exports})
-
-
-def bulk_json_list(request):
-    exports = (
-        DataExport.objects.filter(data_type="json")
-        .select_related("session", "session__jurisdiction")
-        .order_by("session__jurisdiction__name", "session__name")
+    return render(
+        request,
+        "bulk/bulk_session_list.html",
+        {"exports": exports, "data_type": data_type},
     )
-    return render(request, "bulk/bulk_json_list.html", {"exports": exports})
