fd602efeb1753d42561dc91f1bc6f33e663e5a2f
James Turk <dev@jamesturk.net>
2020-02-25 10:38:23 -0500

    bulk csv/json admin

--- bulk/admin.py
+++ bulk/admin.py
@@ -7,10 +7,11 @@ class DataExportAdmin(admin.ModelAdmin):
     list_display = (
         "jurisdiction_name",
         "session_identifier",
+        "data_type",
         "created_at",
         "updated_at",
     )
-    list_filter = ("session__jurisdiction__name",)
+    list_filter = ("data_type", "session__jurisdiction__name")
 
     def jurisdiction_name(self, m):
         return m.session.jurisdiction.name
