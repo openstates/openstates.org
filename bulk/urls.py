7577861c3a01bb3a666d7c4a94a4ca9a1ee930ff
James Turk <dev@jamesturk.net>
2020-03-11 16:53:15 -0400

    /bulk/geo/

--- bulk/urls.py
+++ bulk/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.overview),
     path("legislator-csv/", views.legislator_csv),
+    path("geo/", views.geo),
     path("session-csv/", views.bulk_session_list, {"data_type": "csv"}),
     path("session-json/", views.bulk_session_list, {"data_type": "json"}),
 ]
--- bulk/views.py
+++ bulk/views.py
@@ -14,6 +14,10 @@ def legislator_csv(request):
     )
 
 
+def geo(request):
+    return render(request, "bulk/geo.html", {})
+
+
 def bulk_session_list(request, data_type):
     if request.user.is_anonymous:
         messages.warning(request, "Please log in to access download links.")
