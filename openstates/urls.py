45b4b84e8361446499307b6e1e8102c997666a06
James Turk <dev@jamesturk.net>
2020-03-17 00:28:52 -0400

    remove unused endpoints

--- openstates/urls.py
+++ openstates/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url, include
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
@@ -12,19 +11,18 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("accounts/profile/", include("profiles.urls")),
     path("dashboard/", include("dashboards.urls")),
-    url("", include("geo.urls")),
-    url(r"^api/v1/", include("v1.urls")),
-    url(
-        r"^graphql",
+    path("api/v1/", include("v1.urls")),
+    path(
+        "graphql",
         csrf_exempt(
             KeyedGraphQLView.as_view(
                 graphiql=True, middleware=[QueryProtectionMiddleware(5000)]
             )
         ),
     ),
-    url("", include("public.urls")),
-    url("", include("openstates.redirects")),
-    url("data/", include("bulk.urls")),
+    path("", include("public.urls")),
+    path("", include("openstates.redirects")),
+    path("data/", include("bulk.urls")),
 ]
 
 
@@ -32,7 +30,7 @@ if settings.DEBUG:
     from django.views.defaults import page_not_found, server_error
 
     urlpatterns += [
-        url(r"^404/$", page_not_found, {"exception": None}),
-        url(r"^500/$", server_error),
+        path("404/", page_not_found, {"exception": None}),
+        path("500/", server_error),
         # url(r'^silk/', include('silk.urls', namespace='silk')),
     ]
