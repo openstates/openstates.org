502e26bf2ff5680f49234a831922e2b4d0441f00
James Turk <dev@jamesturk.net>
2020-03-25 15:43:37 -0400

    bundles view WIP

--- bundles/urls.py
+++ bundles/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from .views import bundle_view
 
-urlpatterns = [
-    path("<str:slug>/", bundle_view),
-]
+urlpatterns = [path("<str:slug>/", bundle_view)]
--- bundles/views.py
+++ bundles/views.py
@@ -1,9 +1,27 @@
+from collections import defaultdict
 from .models import Bundle
 from django.shortcuts import render_to_response
 
 
 def bundle_view(request, slug):
     bundle = Bundle.objects.get(slug=slug)
-    bills = bundle.bills.all().order_by("order")
+    bills_by_state = defaultdict(list)
+    total_bills = 0
 
-    return render_to_response("bundles/bundle.html", {"bundle": bundle, "bills": bills})
+    for bill in bundle.bills.all().select_related("legislative_session__jurisdiction"):
+        bills_by_state[bill.legislative_session.jurisdiction.name].append(bill)
+        total_bills += 1
+
+    bills_by_state = {
+        state: sorted(bills, key=lambda b: b.identifier)
+        for state, bills in sorted(bills_by_state.items())
+    }
+
+    return render_to_response(
+        "bundles/bundle.html",
+        {
+            "bundle": bundle,
+            "bills_by_state": bills_by_state,
+            "total_bills": total_bills,
+        },
+    )
