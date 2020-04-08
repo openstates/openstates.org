b662b382be0e940285e5300784cdb35130fd0106
James Turk <dev@jamesturk.net>
2020-04-08 15:40:58 -0400

    init-db changes

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -2,24 +2,8 @@
 
 set -e
 
-WHICH="2020-03"
-FILE=schema.pgdump
-if [ ! -f "$FILE" ]; then
-  wget https://data.openstates.org/postgres/schema/$WHICH-schema.pgdump -O $FILE
-fi
-PGPASSWORD=openstates pg_restore --host db --user openstates -d openstatesorg $FILE;
-# rm $FILE;
-
-
-FILE=data.pgdump
-if [ ! -f "$FILE" ]; then
-  wget https://data.openstates.org/postgres/monthly/$WHICH-public.pgdump -O $FILE
-fi
-
-echo "beginning database restore, will take a while..."
-PGPASSWORD=openstates pg_restore --host db --user openstates -d openstatesorg $FILE;
-# rm $FILE;
-
+poetry run ./manage.py migrate
+DATABASE_URL=postgis://openstates:openstates@db:5432/openstatesorg poetry run os-initdb
 poetry run ./manage.py update_materialized_views --initial
 
 PYTHONPATH=docker/ poetry run ./manage.py shell -c "import testdata"
