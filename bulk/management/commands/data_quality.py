b5c0333e2e051eccd6db67c3aac33d2a4dcf8fb0
schneidy <schneiderman.daniel@gmail.com>
2020-04-22 11:15:35 -0400

    Data Quality: Added lastest action per chamber

--- bulk/management/commands/data_quality.py
+++ bulk/management/commands/data_quality.py
@@ -31,6 +31,8 @@ from openstates.data.models import (
 )
 from utils.common import abbr_to_jid
 from utils.orgs import get_chambers_from_abbr
+from collections import defaultdict
+
 
 
 # Loads the global bill array with all bills from given state and session to use
@@ -39,7 +41,29 @@ def load_bills(state, session):
     sobj = LegislativeSession.objects.get(
         jurisdiction_id=abbr_to_jid(state), identifier=session
     )
-    bills = Bill.objects.filter(legislative_session=sobj)
+    bills = Bill.objects.filter(legislative_session=sobj).prefetch_related("actions")
+    # for bill in Bill.objects.filter(legislative_session=sobj):
+    #     bills.add(bill.select_related(
+    #         "legislative_session",
+    #         "legislative_session__jurisdiction",
+    #         "from_organization",
+    #         "searchable",
+    #     ).prefetch_related(
+    #         "abstracts",
+    #         "other_titles",
+    #         "other_identifiers",
+    #         "actions",
+    #         "related_bills",
+    #         "sponsorships",
+    #         "documents",
+    #         "documents__links",
+    #         "versions",
+    #         "versions__links",
+    #         "sources",
+    #         "votes",
+    #         "votes__counts",
+    #         "votes__votes",
+    #     ))
     return bills
 
 
@@ -50,10 +74,30 @@ def get_available_sessions(state):
     )
 
 def total_bills_per_session(bills, chambers):
-    total_bills_per_session = {}
+    total_bills_per_session = defaultdict(list)
     for chamber in chambers:
+        chamber_name = chamber.name.lower()
         total_bills = bills.filter(from_organization=chamber).count()
-        total_bills_per_session[chamber] = total_bills
+        # Set variables to empty strings in case any info is blank
+        latest_bill = ""
+        latest_action = ""
+        bill_with_latest_action = ""
+
+        print("-------")
+        if total_bills > 0:
+            latest_bill = bills.filter(from_organization=chamber).latest("created_at").created_at
+            bill_with_latest_action = bills.filter(from_organization=chamber).latest("actions__date")
+            if bill_with_latest_action.actions.count() > 0:
+                latest_action = bill_with_latest_action.actions.latest("date")
+                print(latest_action.date + " -- " + latest_action.description)
+
+        total_bills_per_session[chamber_name].append({
+            "chamber": chamber,
+            "total_bills": total_bills,
+            "latest_bill": latest_bill,
+            "bill_with_latest_action": bill_with_latest_action,
+            "latest_action": latest_action}
+        )
     return total_bills_per_session
 
 
