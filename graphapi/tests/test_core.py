522ad78e022aded68bf59176e6f65c17f2646c77
James Turk <dev@jamesturk.net>
2020-04-22 15:25:27 -0400

    fix failing graphqpi test with organization fields that went away

--- graphapi/tests/test_core.py
+++ graphapi/tests/test_core.py
@@ -494,9 +494,9 @@ def test_organization_by_id(django_assert_num_queries):
     )
     sen = Organization.objects.get(jurisdiction__name="Wyoming", classification="upper")
 
-    # 1 query for legislature, 1 query each for children, identifier, names, links, sources
+    # 1 query for legislature, 1 query each for children, links, sources
     # 1 query for senate w/ parent
-    with django_assert_num_queries(8):
+    with django_assert_num_queries(6):
         result = schema.execute(
             """ {
             leg: organization(id: "%s") {
@@ -505,8 +505,6 @@ def test_organization_by_id(django_assert_num_queries):
                 children(classification: "upper", first: 50) {
                     edges { node { classification } }
                 }
-                identifiers { identifier }
-                otherNames { name }
                 links { url }
                 sources { url }
             }
