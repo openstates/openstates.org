e2bef9cf0ec4a55caddc05eac6cdbbede4b934ab
James Turk <dev@jamesturk.net>
2020-04-23 15:14:31 -0400

    docker/init-db is easier to run now

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-set -e
+set -ex
 
-poetry run ./manage.py migrate
-DATABASE_URL=postgis://openstates:openstates@db:5432/openstatesorg poetry run os-initdb
-poetry run ./manage.py update_materialized_views --initial
+# stop database and remove volume
+docker-compose down 
+docker volume rm openstates-postgres
+docker-compose up -d db
+sleep 1
 
-PYTHONPATH=docker/ poetry run ./manage.py shell -c "import testdata"
+# migrate and populate db
+docker-compose run --rm --entrypoint "poetry run ./manage.py migrate" django
+DATABASE_URL=postgis://openstates:openstates@db/openstatesorg docker-compose run --rm --entrypoint "poetry run os-initdb" django
+docker-compose run --rm --entrypoint "poetry run ./manage.py update_materialized_views --initial" django
+
+# add test data
+docker-compose run --rm -e PYTHONPATH=docker/ --entrypoint 'poetry run ./manage.py shell -c "import testdata"' django
