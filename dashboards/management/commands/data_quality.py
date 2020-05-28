e1cc8cfe072cdbbe7d8d00496f4b90c557ce7720
schneidy <schneiderman.daniel@gmail.com>
2020-05-28 17:08:09 -0400

    DQR: Avg num data now uses aggrigates instead of count()

--- dashboards/management/commands/data_quality.py
+++ dashboards/management/commands/data_quality.py
@@ -10,6 +10,7 @@ import pytz
 from collections import defaultdict
 from statistics import mean
 from dashboards.models import DataQualityReport
+from django.db.models import Avg, Max, Min, Count
 
 
 # Loads the global bill array with all bills from given state and session to use
@@ -113,12 +114,29 @@ def average_number_data(bills, chamber):
     min_versions_per_bill = 0
     max_versions_per_bill = 0
 
-    for bill in bills.filter(from_organization=chamber):
-        total_sponsorships_per_bill.append(bill.sponsorships.count())
-        total_actions_per_bill.append(bill.actions.count())
-        total_votes_per_bill.append(bill.votes.count())
-        total_documents_per_bill.append(bill.documents.count())
-        total_versions_per_bill.append(bill.versions.count())
+    test = bills.aggregate(Count("sponsorships"))
+    # test_max = round(max(test))
+    test_bills = bills.filter(from_organization=chamber).annotate(
+        tot_sponsorships=Count("sponsorships"),
+        tot_actions=Count("actions"),
+        tot_documents=Count("documents"),
+        tot_versions=Count("versions"),
+        tot_votes=Count("votes"),
+    )
+
+    # for bill in bills.filter(from_organization=chamber):
+    #     total_sponsorships_per_bill.append(bill.sponsorships.count())
+    #     total_actions_per_bill.append(bill.actions.count())
+    #     total_votes_per_bill.append(bill.votes.count())
+    #     total_documents_per_bill.append(bill.documents.count())
+    #     total_versions_per_bill.append(bill.versions.count())
+
+    for bill in test_bills:
+        total_sponsorships_per_bill.append(bill.tot_sponsorships)
+        total_actions_per_bill.append(bill.tot_actions)
+        total_votes_per_bill.append(bill.tot_votes)
+        total_documents_per_bill.append(bill.tot_documents)
+        total_versions_per_bill.append(bill.tot_versions)
 
     if total_sponsorships_per_bill:
         average_sponsors_per_bill = round(mean(total_sponsorships_per_bill))
@@ -149,15 +167,19 @@ def average_number_data(bills, chamber):
         "average_sponsors_per_bill": average_sponsors_per_bill,
         "min_sponsors_per_bill": min_sponsors_per_bill,
         "max_sponsors_per_bill": max_sponsors_per_bill,
+
         "average_actions_per_bill": average_actions_per_bill,
         "min_actions_per_bill": min_actions_per_bill,
         "max_actions_per_bill": max_actions_per_bill,
+
         "average_votes_per_bill": average_votes_per_bill,
         "min_votes_per_bill": min_votes_per_bill,
         "max_votes_per_bill": max_votes_per_bill,
+
         "average_documents_per_bill": average_documents_per_bill,
         "min_documents_per_bill": min_documents_per_bill,
         "max_documents_per_bill": max_documents_per_bill,
+
         "average_versions_per_bill": average_versions_per_bill,
         "min_versions_per_bill": min_versions_per_bill,
         "max_versions_per_bill": max_versions_per_bill,
