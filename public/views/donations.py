477684eb38d757c551b123cecab91fe4de92e5b2
James Turk <dev@jamesturk.net>
2020-08-06 13:24:23 -0400

    more fixes for new stripe flow

--- public/views/donations.py
+++ public/views/donations.py
@@ -1,7 +1,6 @@
 import stripe
 from django.conf import settings
-from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 
@@ -10,10 +9,15 @@ from django.http import JsonResponse
 def custom_donation(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    amount = int(request.POST["dollars"])
+    # round to nearest dollar
+    try:
+        amount = int(float(request.POST["dollars"]))
+    except ValueError:
+        return JsonResponse({"error": "invalid dollar amount"})
     if amount < 5:
-        messages.error(request, "can not donate less than $5 due to merchant fees")
-        return redirect("/donate/")
+        return JsonResponse(
+            {"error": "can not donate less than $5 due to merchant fees"}
+        )
 
     session = stripe.checkout.Session.create(
         payment_method_types=["card"],
@@ -35,7 +39,7 @@ def custom_donation(request):
 
 
 def donate(request):
-    success = False
+    success = "success" in request.GET
     return render(
         request,
         "flat/donate.html",
