e7963fbbe8bd68b6fd89a3a4c69d9480f54dfce7
James Turk <dev@jamesturk.net>
2020-10-05 16:17:50 -0400

    migration and initial work

--- widgets/models.py
+++ widgets/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class WidgetType(models.TextChoices):
@@ -8,7 +9,11 @@ class WidgetType(models.TextChoices):
 
 class WidgetConfig(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(User, related_name="widgets", on_delete=models.CASCADE)
     widget_type = models.CharField(
-        max_length=3, choices=WidgetType.choices, default=WidgetType.STATE_LEGISLATORS
+        max_length=3, choices=WidgetType.choices, default=WidgetType.STATE_LEGISLATORS,
     )
-    settings = models.JSONField()
+    settings = models.JSONField(blank=True, default=dict)
+
+    def __str__(self):
+        return f"{self.owner} {self.get_widget_type_display()}: {self.id}"
