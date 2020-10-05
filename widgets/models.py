164dd43570aa05ec93a451d9a25c6c25581fe09d
James Turk <dev@jamesturk.net>
2020-10-05 18:15:28 -0400

    more widget details

--- widgets/models.py
+++ widgets/models.py
@@ -10,10 +10,16 @@ class WidgetType(models.TextChoices):
 class WidgetConfig(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     owner = models.ForeignKey(User, related_name="widgets", on_delete=models.CASCADE)
+    name = models.TextField(max_length=50)
     widget_type = models.CharField(
         max_length=3, choices=WidgetType.choices, default=WidgetType.STATE_LEGISLATORS,
     )
     settings = models.JSONField(blank=True, default=dict)
 
+    created_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return f"{self.owner} {self.get_widget_type_display()}: {self.id}"
+        return f"{self.name}: {self.id}"
+
+    def url(self):
+        return f"https://widgets.openstates.org/w/{self.id}"
