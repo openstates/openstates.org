ff467a56e0628bc44554f27aa0987eed98c63a16
James Turk <dev@jamesturk.net>
2020-10-08 11:18:50 -0400

    Revert "set cookie domain"
    This reverts commit 6cea6331493ecad92c9e2ba6c76e38cca6e82adf.

--- web/settings.py
+++ web/settings.py
@@ -59,7 +59,6 @@ elif os.environ.get("DEBUG", "true").lower() == "false":
     RECAPTCHA_PUBLIC_KEY = os.environ["RECAPTCHA_PUBLIC_KEY"]
     RECAPTCHA_PRIVATE_KEY = os.environ["RECAPTCHA_PRIVATE_KEY"]
     RECAPTCHA_USE_SSL = True
-    SESSION_COOKIE_DOMAIN = "openstates.org"
     # enable once SSL is ready
     # SECURE_HSTS_SECONDS = 3600
     # SECURE_SSL_REDIRECT = True
