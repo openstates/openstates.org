1008cbb859437f1d2a07840d3b1a9b6e228ca675
James Turk <dev@jamesturk.net>
2020-12-01 14:12:09 -0500

    remove old donate stuff

--- web/redirects.py
+++ web/redirects.py
@@ -19,6 +19,7 @@ redirects = [
         "https://cdn.forms-content.sg-form.com/b8d934d4-7b67-11ea-a680-1a7f462d56d4",
     ),
     # old flatpages
+    ("donate/", "/about/"),
     ("funding/", "/about/"),
     ("contact/", "/about/"),
     ("bulk/csv/", "/data/session-csv/"),
