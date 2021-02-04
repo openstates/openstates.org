2064b4547dc90b096401c79304568875d9f8f858
James Turk <dev@jamesturk.net>
2021-02-04 16:43:03 -0500

    civic eagle logo

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function checkIE() {
   var ua = window.navigator.userAgent;
   if (ua.indexOf("MSIE") > 0 || ua.indexOf("Trident/") > 0) {
     document.querySelector(".header__logo").src =
-      "/static/images/openstates_logo.png";
+      "/static/images/openstates_ce_logo.png";
     addBanner(
       "You are viewing this site in an outdated browser, some features may not work."
     );
