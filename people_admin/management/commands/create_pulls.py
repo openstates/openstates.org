f2ffb339714ba848ea48008f20fa7adc71609b7f
James Turk <dev@jamesturk.net>
2021-03-08 15:48:13 -0500

    add --update command to pr management util

--- people_admin/management/commands/create_pulls.py
+++ people_admin/management/commands/create_pulls.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from people_admin.models import DeltaSet, PullStatus
-from people_admin.git import delta_set_to_pr
+from people_admin.git import delta_set_to_pr, get_pr_status
 
 
 class Command(BaseCommand):
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--list", default=False, action="store_true")
         parser.add_argument("--delta")
+        parser.add_argument("--update", default=False, action="store_true")
 
     def handle(self, *args, **options):
         nothing = True
@@ -28,5 +29,16 @@ class Command(BaseCommand):
             ds.pr_url = delta_set_to_pr(ds)
             ds.pr_status = PullStatus.CREATED
             ds.save()
+        if options["update"]:
+            nothing = False
+            for ds in DeltaSet.objects.filter(pr_status=PullStatus.CREATED):
+                pr_id = int(ds.pr_url.split("/")[-1])
+                new_status = get_pr_status(pr_id)
+                if new_status != ds.pr_status:
+                    print(
+                        f"Updating {ds.id} | {ds.name} | {ds.get_pr_status} => {new_status}"
+                    )
+                    ds.pr_status = new_status
+                    ds.save()
         if nothing:
             print("must either pass --list or --delta parameters")
