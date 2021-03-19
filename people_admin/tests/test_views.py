c6a5f5308f7d1b646a82a39790de7bc451dffc0a
NewAgeAirbender <paintedflame@gmail.com>
2021-03-19 12:37:46 -0500

    add tests for ignore or source error updates

--- people_admin/tests/test_views.py
+++ people_admin/tests/test_views.py
@@ -16,7 +16,7 @@ def test_apply_match_matches(client, django_assert_num_queries, kansas):
     match_data = (
         '"{"match_data":{"unmatchedId":1,"button":"Match","matchedId":"' + p.id + '"}}"'
     )
-    with django_assert_num_queries(2):
+    with django_assert_num_queries(1):
         # client can be used to mock GET/POST/etc.
         resp = client.post("/admin/people/matcher/update/", match_data)
     assert resp.status_code == 200
@@ -26,6 +26,49 @@ def test_apply_match_matches(client, django_assert_num_queries, kansas):
     matched = UnmatchedName.objects.get()
     assert matched.status == NameStatus.MATCHED_PERSON
     assert matched.matched_person_id == p.id
+
+
+@pytest.mark.django_db
+def test_apply_match_ignore(client, django_assert_num_queries, kansas):
+    session = kansas.legislative_sessions.get(identifier="2020")
+    UnmatchedName.objects.create(
+        id=2, session=session, name="Eva Green", sponsorships_count=16, votes_count=7
+    )
+
+    match_data = '"{"match_data":{"unmatchedId":2,"button":"Ignore","matchedId":""}}"'
+    with django_assert_num_queries(1):
+        # client can be used to mock GET/POST/etc.
+        resp = client.post("/admin/people/matcher/update/", match_data)
+    assert resp.status_code == 200
+    assert resp.json() == {"status": "success"}
+
+    # get refreshed object from database
+    matched = UnmatchedName.objects.get()
+    assert matched.status == NameStatus.IGNORED
+
+
+@pytest.mark.django_db
+def test_apply_match_source_error(client, django_assert_num_queries, kansas):
+    session = kansas.legislative_sessions.get(identifier="2020")
+    UnmatchedName.objects.create(
+        id=3,
+        session=session,
+        name="David Tennant",
+        sponsorships_count=10,
+        votes_count=2,
+    )
+
+    match_data = (
+        '"{"match_data":{"unmatchedId":3,"button":"Source Error","matchedId":""}}"'
+    )
+    with django_assert_num_queries(1):
+        resp = client.post("/admin/people/matcher/update/", match_data)
+    assert resp.status_code == 200
+    assert resp.json() == {"status": "success"}
+
+    # get refreshed object from database
+    matched = UnmatchedName.objects.get()
+    assert matched.status == NameStatus.SOURCE_ERROR
 
 
 @pytest.mark.django_db
