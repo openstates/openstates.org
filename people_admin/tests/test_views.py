3df46b10ed222853817634b934bbd3ad1a98d42b
James Turk <dev@jamesturk.net>
2021-05-27 17:54:54 -0400

    add test for retirement view

--- people_admin/tests/test_views.py
+++ people_admin/tests/test_views.py
@@ -2,8 +2,8 @@ import pytest
 from testutils.factories import create_test_person
 from django.contrib.auth.models import User, Permission
 from openstates.data.models import Person, Organization
-from people_admin.models import UnmatchedName, NameStatus
-from people_admin.views import MATCHER_PERM, EDIT_PERM
+from people_admin.models import UnmatchedName, NameStatus, DeltaSet
+from people_admin.views import MATCHER_PERM, EDIT_PERM, RETIRE_PERM
 import json
 
 
@@ -12,7 +12,9 @@ def admin_user():
     u = User.objects.create(username="admin")
     user_permissions = list(
         Permission.objects.filter(
-            codename__in=[p.split(".")[1] for p in (MATCHER_PERM, EDIT_PERM)]
+            codename__in=[
+                p.split(".")[1] for p in (MATCHER_PERM, EDIT_PERM, RETIRE_PERM)
+            ]
         ).values_list("id", flat=True)
     )
     u.user_permissions.set(user_permissions)
@@ -139,3 +141,36 @@ def test_people_list(client, django_assert_num_queries, admin_user, kansas):
     assert sam_data["district"] == "1"
     assert sam_data["twitter"] == "@SamuelLJackson"
     assert sam_data["capitol_voice"] == "555-555-5555"
+
+
+@pytest.mark.django_db
+def test_retire_person(client, django_assert_num_queries, admin_user, kansas):
+    house = Organization.objects.get(name="Kansas House")
+    sam = create_test_person("Sam Jackson", org=house, party="Democratic", district="1")
+
+    retire_data = {
+        "id": sam.id,
+        "name": sam.name,
+        "reason": "ran for new office",
+        "retirementDate": "2021-01-01",
+        "isDead": False,
+        "vacantSeat": True,
+    }
+    client.force_login(admin_user)
+    with django_assert_num_queries(6):
+        resp = client.post(
+            "/admin/people/retire/",
+            json.dumps(retire_data),
+            content_type="application/json",
+        )
+        assert resp.status_code == 200
+
+    ds = DeltaSet.objects.get()
+    assert "retire Sam Jackson" == ds.name
+    assert ds.person_retirements.all().count() == 1
+    retirement = ds.person_retirements.get()
+    assert retirement.person_id == sam.id
+    assert retirement.reason == "ran for new office"
+    assert retirement.date == "2021-01-01"
+    assert retirement.is_vacant
+    assert retirement.is_dead is False
