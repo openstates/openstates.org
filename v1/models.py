6fdcde2ad7b9172105ddfcdc054675a1c9efa173
James Turk <dev@jamesturk.net>
2021-10-15 15:03:55 -0400

    remove v1 directory

--- v1/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from openstates.data.models import Bill
-
-
-class LegacyBillMapping(models.Model):
-    legacy_id = models.CharField(max_length=20, primary_key=True)
-    bill = models.ForeignKey(
-        Bill, related_name="legacy_mapping", on_delete=models.CASCADE
-    )
