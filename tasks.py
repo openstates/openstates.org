a85d4e2e567ebd86874113db79b8619e15116647
John Seekins <john@civiceagle.com>
2022-09-08 09:27:51 -0600

    bump local python version and run black the way it works for ci
    Signed-off-by: John Seekins <john@civiceagle.com>

--- tasks.py
+++ tasks.py
@@ -45,7 +45,7 @@ def lint(c):
         "poetry run flake8 --show-source --statistics",
         pty=True,
     )
-    c.run("poetry run black --check --diff .", pty=True)
+    c.run("black --check --diff .", pty=True)
 
 
 @task
