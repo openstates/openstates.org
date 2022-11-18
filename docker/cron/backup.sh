396fb6a1079aa8417925e62141148bac6ef20346
John Seekins <john@civiceagle.com>
2022-11-18 15:08:11 -0700

    pg_dump -Fc is compressed already (though this is strangely documented). Piping to gzip would only slow things down
    Signed-off-by: John Seekins <john@civiceagle.com>

--- docker/cron/backup.sh
+++ docker/cron/backup.sh
@@ -3,24 +3,25 @@ set -e
 
 # backup everything to private archive
 echo "Extracting full backup..."
-pg_dump -Fc openstatesorg | gzip > openstatesorg.pgdump.gz
+pg_dump -Fc openstatesorg > openstatesorg.pgdump
 echo "Shipping full backup to s3"
-aws s3 cp openstatesorg.pgdump.gz "s3://openstates-backups/full-backup/$(date +%Y-%m-%d)-openstatesorg.pgdump.gz"
-rm -f openstatesorg.pgdump.gz
+aws s3 cp openstatesorg.pgdump "s3://openstates-backups/full-backup/$(date +%Y-%m-%d)-openstatesorg.pgdump"
+rm -f openstatesorg.pgdump
 
 
 # layered approach for public
 echo "Executing public schema-only backup..."
-pg_dump -Fc openstatesorg --schema-only | gzip > schema.pgdump.gz
+pg_dump -Fc openstatesorg --schema-only > schema.pgdump
+aws s3 cp --acl public-read schema.pgdump "s3://data.openstates.org/postgres/schema/$(date +%Y-%m)-schema.pgdump"
+
 echo "Executing public data backup..."
 pg_dump -Fc openstatesorg --data-only \
   --table=opencivicdata* \
   --table=django_migrations \
   --table=django_content_type \
   --table=django_site \
-  | gzip > public.pgdump.gz
+  > public.pgdump
 
 echo "Uploading public backups to s3..."
-aws s3 cp --acl public-read schema.pgdump.gz "s3://data.openstates.org/postgres/schema/$(date +%Y-%m)-schema.pgdump.gz"
-aws s3 cp --acl public-read public.pgdump.gz "s3://data.openstates.org/postgres/daily/$(date +%Y-%m-%d)-public.pgdump.gz"
-aws s3 cp --acl public-read public.pgdump.gz "s3://data.openstates.org/postgres/monthly/$(date +%Y-%m)-public.pgdump.gz"
+aws s3 cp --acl public-read public.pgdump "s3://data.openstates.org/postgres/daily/$(date +%Y-%m-%d)-public.pgdump"
+aws s3 cp --acl public-read public.pgdump "s3://data.openstates.org/postgres/monthly/$(date +%Y-%m)-public.pgdump"
