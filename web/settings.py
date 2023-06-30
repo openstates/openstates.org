2cb38ed9432ec1f4c20ed41e00c34c2f335a2df6
NewAgeAirbender <34139325+NewAgeAirbender@users.noreply.github.com>
2023-06-30 12:04:53 -0500

    update cookie domain

--- web/settings.py
+++ web/settings.py
@@ -69,7 +69,7 @@ elif os.environ.get("DEBUG", "true").lower() == "false":
     RECAPTCHA_PUBLIC_KEY = os.environ["RECAPTCHA_PUBLIC_KEY"]
     RECAPTCHA_PRIVATE_KEY = os.environ["RECAPTCHA_PRIVATE_KEY"]
     RECAPTCHA_USE_SSL = True
-    SESSION_COOKIE_DOMAIN = "openstates.org"
+    SESSION_COOKIE_DOMAIN = "open.pluralpolicy.com"
     # enable once SSL is ready
     # SECURE_HSTS_SECONDS = 3600
     # SECURE_SSL_REDIRECT = True
