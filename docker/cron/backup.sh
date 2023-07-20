5ef904da8d6be6d3e87445efac9060e8a0d1e285
John Seekins <john@pluralpolicy.com>
2023-07-20 08:11:39 -0600

    disable geo backup for now. bad permissions
    Signed-off-by: John Seekins <john@pluralpolicy.com>

--- docker/cron/backup.sh
+++ docker/cron/backup.sh
@@ -18,16 +18,11 @@ echo "Shipping full backup to s3"
 aws s3 cp openstatesorg.pgdump "s3://openstates-backups/full-backup/$(date +%Y-%m-%d)-openstatesorg.pgdump" > /dev/null
 rm -f openstatesorg.pgdump
 
-echo "Extracting geo backup..."
-pg_dump -Fc geo > openstates-geo.pgdump
-echo "Shipping full backup to s3"
-aws s3 cp openstates-geo.pgdump "s3://openstates-backups/full-backup/$(date +%Y-%m-%d)-openstates-geo.pgdump" > /dev/null
-rm -f openstates-geo.pgdump
-
 # layered approach for public
 echo "Executing public schema-only backup..."
 pg_dump -Fc openstatesorg --schema-only > schema.pgdump
 aws s3 cp --acl public-read schema.pgdump "s3://data.openstates.org/postgres/schema/$(date +%Y-%m)-schema.pgdump" > /dev/null
+rm -f schema.pgdump
 
 echo "Executing public data backup..."
 pg_dump -Fc openstatesorg --data-only \
@@ -40,3 +35,10 @@ pg_dump -Fc openstatesorg --data-only \
 echo "Uploading public backups to s3..."
 aws s3 cp --acl public-read public.pgdump "s3://data.openstates.org/postgres/daily/$(date +%Y-%m-%d)-public.pgdump" > /dev/null
 aws s3 cp --acl public-read public.pgdump "s3://data.openstates.org/postgres/monthly/$(date +%Y-%m)-public.pgdump" > /dev/null
+rm -f public.pgdump
+
+#echo "Extracting geo backup..."
+#pg_dump -Fc geo > openstates-geo.pgdump
+#echo "Shipping full backup to s3"
+#aws s3 cp openstates-geo.pgdump "s3://openstates-backups/full-backup/$(date +%Y-%m-%d)-openstates-geo.pgdump" > /dev/null
+#rm -f openstates-geo.pgdump
