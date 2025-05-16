3a6d933b185e8baf949f38f722032e8f98be6354
Jesse Mortenson <jessemortenson@gmail.com>
2025-05-15 22:24:59 -0500

    Retire legislator view tests

--- public/tests/test_legislator_views.py
+++ public/tests/test_legislator_views.py
@@ -20,63 +20,64 @@ def test_legislators_view(client, django_assert_num_queries):
     assert len(resp.context["legislators"]) == 6
 
 
-@pytest.mark.django_db
-def test_person_view(client, django_assert_num_queries):
-    p = Person.objects.get(name="Amanda Adams")
-    with django_assert_num_queries(9):
-        resp = client.get(pretty_url(p))
-    assert resp.status_code == 200
-    assert resp.context["state"] == "ak"
-    assert resp.context["state_nav"] == "legislators"
-    person = resp.context["person"]
-    assert person.name == "Amanda Adams"
-    assert person.primary_party == "Republican"
-    assert (
-        person.current_jurisdiction_id
-        == "ocd-jurisdiction/country:us/state:ak/government"
-    )
-    assert person.current_role == {
-        "org_classification": "lower",
-        "district": 1,
-        "division_id": "ocd-division/country:us/state:ak/sldl:1",
-        "title": "Representative",
-    }
-    assert len(person.sponsored_bills) == 2
-    assert len(person.vote_events) == 1
-    assert resp.context["retired"] is False
-
-
-@pytest.mark.django_db
-def test_person_view_retired(client, django_assert_num_queries):
-    p = Person.objects.get(name="Rhonda Retired")
-    # fewer views, we don't do the bill queries
-    with django_assert_num_queries(9):
-        resp = client.get(pretty_url(p))
-    assert resp.status_code == 200
-    assert resp.context["state"] == "ak"
-    assert resp.context["state_nav"] == "legislators"
-    person = resp.context["person"]
-    assert person.name == "Rhonda Retired"
-    assert resp.context["retired"] is True
-
-
-@pytest.mark.django_db
-def test_person_view_invalid_uuid(client, django_assert_num_queries):
-    p = Person.objects.get(name="Rhonda Retired")
-    resp = client.get(
-        pretty_url(p)[:-1] + "abcdefghij/"
-    )  # this won't be a valid pretty UUID
-    assert resp.status_code == 404
-
-
-@pytest.mark.django_db
-def test_canonicalize_person(client):
-    p = Person.objects.get(name="Amanda Adams")
-    url = pretty_url(p).replace("amanda", "xyz")
-    assert "xyz" in url
-    resp = client.get(url)
-    assert resp.status_code == 301
-    assert resp.url == pretty_url(p)
+# Person view is disabled in favor of Plural app
+# @pytest.mark.django_db
+# def test_person_view(client, django_assert_num_queries):
+#     p = Person.objects.get(name="Amanda Adams")
+#     with django_assert_num_queries(9):
+#         resp = client.get(pretty_url(p))
+#     assert resp.status_code == 200
+#     assert resp.context["state"] == "ak"
+#     assert resp.context["state_nav"] == "legislators"
+#     person = resp.context["person"]
+#     assert person.name == "Amanda Adams"
+#     assert person.primary_party == "Republican"
+#     assert (
+#         person.current_jurisdiction_id
+#         == "ocd-jurisdiction/country:us/state:ak/government"
+#     )
+#     assert person.current_role == {
+#         "org_classification": "lower",
+#         "district": 1,
+#         "division_id": "ocd-division/country:us/state:ak/sldl:1",
+#         "title": "Representative",
+#     }
+#     assert len(person.sponsored_bills) == 2
+#     assert len(person.vote_events) == 1
+#     assert resp.context["retired"] is False
+#
+#
+# @pytest.mark.django_db
+# def test_person_view_retired(client, django_assert_num_queries):
+#     p = Person.objects.get(name="Rhonda Retired")
+#     # fewer views, we don't do the bill queries
+#     with django_assert_num_queries(9):
+#         resp = client.get(pretty_url(p))
+#     assert resp.status_code == 200
+#     assert resp.context["state"] == "ak"
+#     assert resp.context["state_nav"] == "legislators"
+#     person = resp.context["person"]
+#     assert person.name == "Rhonda Retired"
+#     assert resp.context["retired"] is True
+#
+#
+# @pytest.mark.django_db
+# def test_person_view_invalid_uuid(client, django_assert_num_queries):
+#     p = Person.objects.get(name="Rhonda Retired")
+#     resp = client.get(
+#         pretty_url(p)[:-1] + "abcdefghij/"
+#     )  # this won't be a valid pretty UUID
+#     assert resp.status_code == 404
+#
+#
+# @pytest.mark.django_db
+# def test_canonicalize_person(client):
+#     p = Person.objects.get(name="Amanda Adams")
+#     url = pretty_url(p).replace("amanda", "xyz")
+#     assert "xyz" in url
+#     resp = client.get(url)
+#     assert resp.status_code == 301
+#     assert resp.url == pretty_url(p)
 
 
 # TODO: test find_your_legislator
